feaea8f3c82f4b06e5388b9fb481d18dd2686c26
m.kishi <m.kishi.5@gmail.com>
2013-01-12 21:46:10 +0900

    ADD:ヘッダーコントロール

---
+++ Abook/src/control/AbHeaderControl.cs
@@ -0,0 +1,69 @@
+﻿namespace Abook
+{
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// ヘッダーコントロール
+    /// </summary>
+    public partial class AbHeaderControl : UserControl
+    {
+        /// <summary>前年ボタンクリック</summary>
+        public event EventHandler PrevYearClick;
+        /// <summary>前月ボタンクリック</summary>
+        public event EventHandler PrevMonthClick;
+        /// <summary>翌月ボタンクリック</summary>
+        public event EventHandler NextMonthClick;
+        /// <summary>翌年ボタンクリック</summary>
+        public event EventHandler NextYearClick;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public AbHeaderControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// タイトル
+        /// </summary>
+        public string Title
+        {
+            get { return LblTitle.Text; }
+            set { LblTitle.Text = value; }
+        }
+
+        /// <summary>
+        /// 前年ボタンクリック
+        /// </summary>
+        private void BtnPrevYear_Click(object sender, EventArgs e)
+        {
+            PrevYearClick(sender, e);
+        }
+
+        /// <summary>
+        /// 前月ボタンクリック
+        /// </summary>
+        private void BtnPrevMonth_Click(object sender, EventArgs e)
+        {
+            PrevMonthClick(sender, e);
+        }
+
+        /// <summary>
+        /// 翌月ボタンクリック
+        /// </summary>
+        private void BtnNextMonth_Click(object sender, EventArgs e)
+        {
+            NextMonthClick(sender, e);
+        }
+
+        /// <summary>
+        /// 翌年ボタンクリック
+        /// </summary>
+        private void BtnNextYear_Click(object sender, EventArgs e)
+        {
+            NextYearClick(sender, e);
+        }
+    }
+}
