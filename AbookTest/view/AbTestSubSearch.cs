6ec6e19048f47bf9c070edd84c0b9c12b691212a
m.kishi <m.kishi.5@gmail.com>
2019-12-24 22:41:24 +0900

    ADD:検索サブフォームに種別条件を追加

--- AbookTest/view/AbTestSubSearch.cs
+++ AbookTest/view/AbTestSubSearch.cs
@@ -67,6 +67,11 @@ namespace AbookTest
                 sw.WriteLine(TT.ToCSV("2017-12-03", "おにぎりＡ"  , TYPE.FOOD, "300"));
                 sw.WriteLine(TT.ToCSV("2017-12-04", "Ｂおにぎり"  , TYPE.FOOD, "400"));
                 sw.WriteLine(TT.ToCSV("2017-12-05", "ＣおにぎりＤ", TYPE.FOOD, "500"));
+                sw.WriteLine(TT.ToCSV("2019-12-21", "おにぎり"    , TYPE.OTFD, "101"));
+                sw.WriteLine(TT.ToCSV("2019-12-22", "おにぎり"    , TYPE.OTFD, "202"));
+                sw.WriteLine(TT.ToCSV("2019-12-23", "おにぎりＡ"  , TYPE.TRFC, "303"));
+                sw.WriteLine(TT.ToCSV("2019-12-24", "Ｂおにぎり"  , TYPE.FRND, "404"));
+                sw.WriteLine(TT.ToCSV("2019-12-25", "ＣおにぎりＤ", TYPE.INSU, "505"));
                 sw.Close();
             }
         }
@@ -104,7 +109,7 @@ namespace AbookTest
         }
 
         /// <summary>
-        /// コンボボックス取得
+        /// コンボボックス取得(名称)
         /// </summary>
         /// <returns>コンボボックス</returns>
         protected ComboBox CtCmbName()
@@ -113,12 +118,30 @@ namespace AbookTest
         }
 
         /// <summary>
-        /// コンボボックス取得
+        /// コンボボックス取得(名称)
         /// </summary>
         /// <returns>コンボボックス</returns>
         protected ComboBoxTester TsCmbName()
         {
             return new ComboBoxTester("CmbName", form);
+        }
+
+        /// <summary>
+        /// コンボボックス取得(種別)
+        /// </summary>
+        /// <returns>コンボボックス</returns>
+        protected ComboBox CtCmbType()
+        {
+            return (new Finder<ComboBox>("CmbType", form).Find());
+        }
+
+        /// <summary>
+        /// コンボボックス取得(種別)
+        /// </summary>
+        /// <returns>コンボボックス</returns>
+        protected ComboBoxTester TsCmbType()
+        {
+            return new ComboBoxTester("CmbType", form);
         }
 
         /// <summary>
@@ -222,6 +245,27 @@ namespace AbookTest
         }
 
         /// <summary>
+        /// コンボボックステスト
+        /// 種別
+        /// </summary>
+        [Test, RequiresSTA]
+        public void LoadWithType()
+        {
+            ShowSubSearch(CSV_EMPTY);
+
+            var cmbType = CtCmbType();
+
+            // +1は空白
+            Assert.AreEqual(TYPE.EXPENCE.Length + 1, cmbType.Items.Count);
+
+            Assert.AreEqual("", cmbType.Items[0]);
+            for (int i = 0; i < TYPE.EXPENCE.Length; i++)
+            {
+                Assert.AreEqual(TYPE.EXPENCE[i], cmbType.Items[i + 1]);
+            }
+        }
+
+        /// <summary>
         /// レコード数のテスト
         /// 初期表示: 0件
         /// </summary>
@@ -248,7 +292,7 @@ namespace AbookTest
             TsBtnSearch().Click();
 
             var dgvExpense = CtDgvExpense();
-            Assert.AreEqual(5, dgvExpense.Rows.Count);
+            Assert.AreEqual(10, dgvExpense.Rows.Count);
             Assert.AreEqual("2017-12-01", dgvExpense.Rows[0].Cells[0].Value);
             Assert.AreEqual("2017-12-02", dgvExpense.Rows[1].Cells[0].Value);
             Assert.AreEqual("2017-12-03", dgvExpense.Rows[2].Cells[0].Value);
@@ -259,6 +303,17 @@ namespace AbookTest
             Assert.AreEqual("おにぎりＡ"  , dgvExpense.Rows[2].Cells[1].Value);
             Assert.AreEqual("Ｂおにぎり"  , dgvExpense.Rows[3].Cells[1].Value);
             Assert.AreEqual("ＣおにぎりＤ", dgvExpense.Rows[4].Cells[1].Value);
+
+            Assert.AreEqual("2019-12-21", dgvExpense.Rows[5].Cells[0].Value);
+            Assert.AreEqual("2019-12-22", dgvExpense.Rows[6].Cells[0].Value);
+            Assert.AreEqual("2019-12-23", dgvExpense.Rows[7].Cells[0].Value);
+            Assert.AreEqual("2019-12-24", dgvExpense.Rows[8].Cells[0].Value);
+            Assert.AreEqual("2019-12-25", dgvExpense.Rows[9].Cells[0].Value);
+            Assert.AreEqual("おにぎり"    , dgvExpense.Rows[5].Cells[1].Value);
+            Assert.AreEqual("おにぎり"    , dgvExpense.Rows[6].Cells[1].Value);
+            Assert.AreEqual("おにぎりＡ"  , dgvExpense.Rows[7].Cells[1].Value);
+            Assert.AreEqual("Ｂおにぎり"  , dgvExpense.Rows[8].Cells[1].Value);
+            Assert.AreEqual("ＣおにぎりＤ", dgvExpense.Rows[9].Cells[1].Value);
         }
 
         /// <summary>
@@ -296,11 +351,17 @@ namespace AbookTest
 
             var dgvExpense = CtDgvExpense();
             Assert.AreEqual(2, cmbName.SelectedIndex);
-            Assert.AreEqual(2, dgvExpense.Rows.Count);
+            Assert.AreEqual(4, dgvExpense.Rows.Count);
             Assert.AreEqual("2017-12-01", dgvExpense.Rows[0].Cells[0].Value);
             Assert.AreEqual("2017-12-02", dgvExpense.Rows[1].Cells[0].Value);
-            Assert.AreEqual("おにぎり", dgvExpense.Rows[0].Cells[1].Value);
-            Assert.AreEqual("おにぎり", dgvExpense.Rows[1].Cells[1].Value);
+            Assert.AreEqual("おにぎり"  , dgvExpense.Rows[0].Cells[1].Value);
+            Assert.AreEqual("おにぎり"  , dgvExpense.Rows[1].Cells[1].Value);
+
+            Assert.AreEqual("2019-12-21", dgvExpense.Rows[2].Cells[0].Value);
+            Assert.AreEqual("2019-12-22", dgvExpense.Rows[3].Cells[0].Value);
+            Assert.AreEqual("おにぎり"  , dgvExpense.Rows[2].Cells[1].Value);
+            Assert.AreEqual("おにぎり"  , dgvExpense.Rows[3].Cells[1].Value);
+
         }
 
         /// <summary>
@@ -315,6 +376,145 @@ namespace AbookTest
             var cmbName = CtCmbName();
             cmbName.Text = "おにぎり";
             cmbName.SelectedIndex = -1;
+
+            TsBtnSearch().Click();
+
+            var dgvExpense = CtDgvExpense();
+            Assert.AreEqual(10, dgvExpense.Rows.Count);
+            Assert.AreEqual("2017-12-01", dgvExpense.Rows[0].Cells[0].Value);
+            Assert.AreEqual("2017-12-02", dgvExpense.Rows[1].Cells[0].Value);
+            Assert.AreEqual("2017-12-03", dgvExpense.Rows[2].Cells[0].Value);
+            Assert.AreEqual("2017-12-04", dgvExpense.Rows[3].Cells[0].Value);
+            Assert.AreEqual("2017-12-05", dgvExpense.Rows[4].Cells[0].Value);
+            Assert.AreEqual("おにぎり"    , dgvExpense.Rows[0].Cells[1].Value);
+            Assert.AreEqual("おにぎり"    , dgvExpense.Rows[1].Cells[1].Value);
+            Assert.AreEqual("おにぎりＡ"  , dgvExpense.Rows[2].Cells[1].Value);
+            Assert.AreEqual("Ｂおにぎり"  , dgvExpense.Rows[3].Cells[1].Value);
+            Assert.AreEqual("ＣおにぎりＤ", dgvExpense.Rows[4].Cells[1].Value);
+
+            Assert.AreEqual("2019-12-21", dgvExpense.Rows[5].Cells[0].Value);
+            Assert.AreEqual("2019-12-22", dgvExpense.Rows[6].Cells[0].Value);
+            Assert.AreEqual("2019-12-23", dgvExpense.Rows[7].Cells[0].Value);
+            Assert.AreEqual("2019-12-24", dgvExpense.Rows[8].Cells[0].Value);
+            Assert.AreEqual("2019-12-25", dgvExpense.Rows[9].Cells[0].Value);
+            Assert.AreEqual("おにぎり"    , dgvExpense.Rows[5].Cells[1].Value);
+            Assert.AreEqual("おにぎり"    , dgvExpense.Rows[6].Cells[1].Value);
+            Assert.AreEqual("おにぎりＡ"  , dgvExpense.Rows[7].Cells[1].Value);
+            Assert.AreEqual("Ｂおにぎり"  , dgvExpense.Rows[8].Cells[1].Value);
+            Assert.AreEqual("ＣおにぎりＤ", dgvExpense.Rows[9].Cells[1].Value);
+        }
+
+        /// <summary>
+        /// 支出検索
+        /// 検索条件: 部分一致(前方一致)
+        /// </summary>
+        [Test, RequiresSTA]
+        public void SearchWithPartialMatchForward()
+        {
+            ShowSubSearch(CSV_EXIST);
+
+            var cmbName = CtCmbName();
+            cmbName.SelectedIndex = -1;
+            cmbName.Text = "Ｂ";
+
+            TsBtnSearch().Click();
+
+            var dgvExpense = CtDgvExpense();
+            Assert.AreEqual(2, dgvExpense.Rows.Count);
+            Assert.AreEqual("2017-12-04", dgvExpense.Rows[0].Cells[0].Value);
+            Assert.AreEqual("Ｂおにぎり", dgvExpense.Rows[0].Cells[1].Value);
+
+            Assert.AreEqual("2019-12-24", dgvExpense.Rows[1].Cells[0].Value);
+            Assert.AreEqual("Ｂおにぎり", dgvExpense.Rows[1].Cells[1].Value);
+        }
+
+        /// <summary>
+        /// 支出検索
+        /// 検索条件: 部分一致(後方一致)
+        /// </summary>
+        [Test, RequiresSTA]
+        public void SearchWithPartialMatchBackward()
+        {
+            ShowSubSearch(CSV_EXIST);
+
+            var cmbName = CtCmbName();
+            cmbName.SelectedIndex = -1;
+            cmbName.Text = "Ｄ";
+
+            TsBtnSearch().Click();
+
+            var dgvExpense = CtDgvExpense();
+            Assert.AreEqual(2, dgvExpense.Rows.Count);
+            Assert.AreEqual("2017-12-05", dgvExpense.Rows[0].Cells[0].Value);
+            Assert.AreEqual("ＣおにぎりＤ", dgvExpense.Rows[0].Cells[1].Value);
+
+            Assert.AreEqual("2019-12-25", dgvExpense.Rows[1].Cells[0].Value);
+            Assert.AreEqual("ＣおにぎりＤ", dgvExpense.Rows[1].Cells[1].Value);
+        }
+
+        /// <summary>
+        /// 支出検索
+        /// 検索条件: 種別指定
+        /// </summary>
+        [Test, RequiresSTA]
+        public void SearchWithType()
+        {
+            ShowSubSearch(CSV_EXIST);
+
+            var cmbName = CtCmbName();
+            cmbName.SelectedIndex = -1;
+            cmbName.Text = "";
+
+            var cmbType = CtCmbType();
+            cmbType.SelectedIndex = 2;
+
+            TsBtnSearch().Click();
+
+            var dgvExpense = CtDgvExpense();
+            Assert.AreEqual(2, dgvExpense.Rows.Count);
+            Assert.AreEqual("2019-12-21", dgvExpense.Rows[0].Cells[0].Value);
+            Assert.AreEqual("2019-12-22", dgvExpense.Rows[1].Cells[0].Value);
+            Assert.AreEqual("おにぎり"  , dgvExpense.Rows[0].Cells[1].Value);
+            Assert.AreEqual("おにぎり"  , dgvExpense.Rows[1].Cells[1].Value);
+        }
+
+        /// <summary>
+        /// 支出検索
+        /// 検索条件: ヒットなし
+        /// </summary>
+        [Test, RequiresSTA]
+        public void SearchWithTypeNotFound()
+        {
+            ShowSubSearch(CSV_EXIST);
+
+            var cmbName = CtCmbName();
+            cmbName.SelectedIndex = -1;
+            cmbName.Text = "";
+
+            var cmbType = CtCmbType();
+            cmbType.SelectedIndex = 6;
+
+            TsBtnSearch().Click();
+
+            var dgvExpense = CtDgvExpense();
+            Assert.AreEqual(0, dgvExpense.Rows.Count);
+        }
+
+        /// <summary>
+        /// 支出検索
+        /// 検索条件: 名称(部分一致)+種別
+        /// </summary>
+        [Test, RequiresSTA]
+        public void SearchWithPartialNameAndType()
+        {
+            ShowSubSearch(CSV_EXIST);
+
+            var cmbName = CtCmbName();
+            cmbName.Text = "おにぎり";
+            cmbName.SelectedIndex = -1;
+
+            var cmbType = CtCmbType();
+            cmbType.SelectedIndex = 1;
 
             TsBtnSearch().Click();
 
@@ -334,44 +534,25 @@ namespace AbookTest
 
         /// <summary>
         /// 支出検索
-        /// 検索条件: 部分一致(前方一致)
+        /// 検索条件: 名称(完全)+種別
         /// </summary>
         [Test, RequiresSTA]
-        public void SearchWithPartialMatchForward()
+        public void SearchWithMatchNameAndType()
         {
             ShowSubSearch(CSV_EXIST);
 
             var cmbName = CtCmbName();
-            cmbName.SelectedIndex = -1;
-            cmbName.Text = "Ｂ";
+            cmbName.SelectedIndex = 3;
+
+            var cmbType = CtCmbType();
+            cmbType.SelectedIndex = 1;
 
             TsBtnSearch().Click();
 
             var dgvExpense = CtDgvExpense();
             Assert.AreEqual(1, dgvExpense.Rows.Count);
-            Assert.AreEqual("2017-12-04", dgvExpense.Rows[0].Cells[0].Value);
-            Assert.AreEqual("Ｂおにぎり", dgvExpense.Rows[0].Cells[1].Value);
-        }
-
-        /// <summary>
-        /// 支出検索
-        /// 検索条件: 部分一致(後方一致)
-        /// </summary>
-        [Test, RequiresSTA]
-        public void SearchWithPartialMatchBackward()
-        {
-            ShowSubSearch(CSV_EXIST);
-
-            var cmbName = CtCmbName();
-            cmbName.SelectedIndex = -1;
-            cmbName.Text = "Ｄ";
-
-            TsBtnSearch().Click();
-
-            var dgvExpense = CtDgvExpense();
-            Assert.AreEqual(1, dgvExpense.Rows.Count);
-            Assert.AreEqual("2017-12-05", dgvExpense.Rows[0].Cells[0].Value);
-            Assert.AreEqual("ＣおにぎりＤ", dgvExpense.Rows[0].Cells[1].Value);
+            Assert.AreEqual("2017-12-03", dgvExpense.Rows[0].Cells[0].Value);
+            Assert.AreEqual("おにぎりＡ", dgvExpense.Rows[0].Cells[1].Value);
         }
 
         /// <summary>
